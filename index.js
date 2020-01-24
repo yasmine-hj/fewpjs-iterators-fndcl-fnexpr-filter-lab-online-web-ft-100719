4ad90dd34132eb22695438bf7673c8ef1cc2c383
yasmine-hj <yasminehajaji95@gmail.com>
2020-01-24 14:14:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+
+function findMatching (drivers, outcome) {
+    return drivers.filter(driver =>
+        driver.toLowerCase() === outcome.toLowerCase()
+)};
+
+function fuzzyMatch (drivers, searchTerm) {
+    return drivers.filter(driver =>
+        driver.toLowerCase().indexOf(searchTerm.toLowerCase()) === 0
+)};
+
+function matchName (drivers, searchTerm) {
+    return drivers.filter(driver => 
+        driver.name === searchTerm
+)};
+
